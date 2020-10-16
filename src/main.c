ccb3eee257050885484d7cd446871b6469c69b2f
Ankur Seth <iankurseth@gmail.com>
2020-10-16 15:12:03 +0530

    Now reading file into string

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "julius.h"
 
@@ -12,11 +13,17 @@ int main (int argc, char const *argv[]) {
 		if (fp == NULL) {
 			printf("File not found.\n");
 		} else {
-			char ch;
-			while ((ch = fgetc(fp)) != EOF) {
-				printf("%c", ch);
-			}
-			printf("\n");
+			long p1 = ftell(fp);
+			fseek(fp, p1, SEEK_END);
+			long p2 = ftell(fp);
+			fseek(fp, p1, SEEK_SET);
+			long size = 1 + p2 - p1;
+
+			char *text = malloc(sizeof(char) * (size + 1));
+			fread(text, sizeof(char), size, fp);
+			text[size] = '\0';
+			printf("%s\n", text);
+			free(text);
 			fclose(fp);
 		}
 	}
