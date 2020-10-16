736f5e1f88dbda97f97afc0ba5a5e73ed0d4a742
Ankur Seth <iankurseth@gmail.com>
2020-10-16 14:38:34 +0530

    Now printing file

--- src/main.c
+++ src/main.c
@@ -8,6 +8,16 @@ int main (int argc, char const *argv[]) {
 	} else if (argc > 2) {
 		printf("Too many args!\n");
 	} else {
-		printf("Exactly two args.\n");
+		FILE *fp = fopen(argv[1], "r");
+		if (fp == NULL) {
+			printf("File not found.\n");
+		} else {
+			char ch;
+			while ((ch = fgetc(fp)) != EOF) {
+				printf("%c", ch);
+			}
+			printf("\n");
+			fclose(fp);
+		}
 	}
 }
