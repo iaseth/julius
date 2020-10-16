4166250089e108c81fff7c48bdfd23fa0a550a59
Ankur Seth <iankurseth@gmail.com>
2020-10-16 15:50:44 +0530

    Printing line numbers

--- lib/julius.c
+++ lib/julius.c
@@ -9,6 +9,9 @@ void parse_json (char *text) {
 	int quoted_text_index = 0;
 	int inside_quote = 0;
 
+	int row_number = 0;
+	int col_number = 0;
+
 	int index = 0;
 	while (text[index] != '\0') {
 		ch = text[index];
@@ -16,7 +19,7 @@ void parse_json (char *text) {
 			if (ch == quote) {
 				quoted_text[quoted_text_index] = '\0';
 				inside_quote = 0;
-				printf("Quoted text: '%s'\n", quoted_text);
+				printf("Quoted text: L%d. '%s'\n", (row_number+1), quoted_text);
 			} else {
 				quoted_text[quoted_text_index++] = ch;
 			}
@@ -27,6 +30,12 @@ void parse_json (char *text) {
 			quote = ch;
 		}
 
+		if (ch == '\n') {
+			row_number++;
+			col_number = 0;
+		} else {
+			col_number++;
+		}
 		//printf("%c", ch);
 		index++;
 	}
